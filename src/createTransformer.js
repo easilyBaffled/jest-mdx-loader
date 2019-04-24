33f627a79f252a9b038628e649c6624cb35eba1f
Dan Michaelis <dmichaelis0+github@gmail.com>
2019-04-24 16:06:16 -0400

    user modern babel presets

--- src/createTransformer.js
+++ src/createTransformer.js
@@ -1,34 +1,30 @@
 var mdx = require("@mdx-js/mdx");
-var babel = require("babel-core");
+var babel = require("@babel/core");
 
 createTransformer = function(preMdxParseCallback) {
   return function(src, filename, config, options) {
     let rawMDX = src;
 
-    if (preMdxParseCallback) {
-      rawMDX = preMdxParseCallback(rawMDX);
+    if ( preMdxParseCallback )
+    {
+      rawMDX = preMdxParseCallback( rawMDX );
     }
 
     // Convert .MDX file into JSX
-    var rawJSX = mdx.sync(rawMDX);
+    var rawJSX = mdx.sync( rawMDX );
 
     // Inject React and MDXTag imports
     var injectedJSX =
-      "import React from 'react'; import MDXTag from '@mdx-js/tag/dist/mdx-tag';" +
-      rawJSX;
+        "import React from 'react'; import MDXTag from '@mdx-js/tag/dist/mdx-tag';" +
+        rawJSX.replace("const MDXLayout = \"wrapper\"", "const MDXLayout = React.Fragment\n");
 
     // Transform ES6 with babel
-    var babelRes = babel.transform(injectedJSX, {
-      presets: ["env", "react"],
-      plugins: [
-        "transform-class-properties",
-        "transform-object-rest-spread",
-        "react-hot-loader/babel"
-      ]
-    }).code;
-
-    return babelRes;
-  };
+    const res = babel.transform( injectedJSX, {
+      presets: [ "@babel/preset-env", "react-app" ]
+    } );
+    return res;
+    
+  }
 };
 
 module.exports = createTransformer;
